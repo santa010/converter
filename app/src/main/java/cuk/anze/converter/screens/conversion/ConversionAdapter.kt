46d45dbc7274b299ce60ae42d9a2588871ab8371
anzeC <cukanze@gmail.com>
2019-08-19 16:18:03 +0200

    Move view extensions to seperate file

--- app/src/main/java/cuk/anze/converter/screens/conversion/ConversionAdapter.kt
+++ app/src/main/java/cuk/anze/converter/screens/conversion/ConversionAdapter.kt
@@ -1,21 +1,20 @@
 package cuk.anze.converter.screens.conversion
 
-import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import cuk.anze.converter.R
+import cuk.anze.converter.extensions.showKeyboard
 import cuk.anze.converter.model.CurrencyInfo
 import kotlinx.android.synthetic.main.conversion_row.view.*
 import java.lang.Double.parseDouble
@@ -184,18 +183,4 @@ class ConversionAdapter(private val presenter: ConverterContract.Presenter): Rec
             }
         }
     }
-}
-
-fun View.showKeyboard() {
-    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    this.requestFocus()
-    imm.showSoftInput(this, 0)
-}
-
-fun View.hideKeyboard(): Boolean {
-    try {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        return inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
-    } catch (ignored: RuntimeException) { }
-    return false
 }
